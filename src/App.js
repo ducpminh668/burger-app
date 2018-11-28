c5c161002412711b6c41a590bedc4c2324ef90c8
ducky <ducky@DucPham.connectify>
2018-11-28 16:57:49 +0700

    test module

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import React, { Component } from 'react';
 import './App.css';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import classes from './App.module.css';
 
 class App extends Component {
   render() {
     return (
-     <div>
+     <div className={classes.Test}>
        <Layout>
          <BurgerBuilder />
        </Layout>
